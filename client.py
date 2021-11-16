82ba85894bda94a8d9d55be9b4e65ac72a4571e1
yossix07 <79411610+yossix07@users.noreply.github.com>
2021-11-17 00:22:35 +0200

    add Event class to client - starting "connect" func in client

--- client.py
+++ client.py
@@ -4,6 +4,64 @@ import string
 import random
 import time
 import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Event:
+    def __init__(self, file, time, action):
+        self.file = file
+        self.time = time
+        self.action = action
+
+    def run(self):
+        # TODO - run each action
+        pass
+
+
+class Watcher:
+    def __init__(self, folder_path, queue):
+        self.observer = Observer()
+        self.folder = folder_path
+        self.queue = queue
+
+    def run(self):
+        event_handler = Handler(self.queue)
+        self.observer.schedule(event_handler, self.folder, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+    patterns = ["*.fits"]
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def on_created(self, event):
+        self.queue.append(Event(event.src_path, time.time(), "create"))
+        print("Watchdog received created event - % s." % event.src_path)
+        # Event is created, you can process it now
+
+    def on_modified(self, event):
+        self.queue.append(Event(event.src_path, time.time(), "modify"))
+        print("Watchdog received modified event - % s." % event.src_path)
+        # Event is modified, you can process it now
+
+    def on_moved(self, event):
+        self.queue.append(Event(event.src_path, time.time(), "move"))
+        print("Watchdog received moved event - % s." % event.src_path)
+
+    def on_deleted(self, event):
+        self.queue.append(Event(event.src_path, time.time(), "delete"))
+        print("Watchdog received delete event - % s." % event.src_path)
 
 
 class CONST:
@@ -73,8 +131,22 @@ def sign_to_server(server_ip, server_port, folder_path, refresh_rate):
 
 
 def connect(server_ip, server_port, folder_patch, refresh_rate, id_number):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((server_ip, int(server_port)))
+    queue = []
+
+    # watch the folder and enter events to queue
+    watchdog = Watcher(folder_patch, queue)
+
+    while True:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((server_ip, int(server_port)))
+        with server_socket:
+            # send a 1 flag to re-connect as an existing client
+            server_socket.send(b'1')
+
+            # send id to server
+            server_socket.send(id_number.encode("utf-8"))
+
+        # time.sleep(refresh_rate)
 
 
 # check if the received ip address is in correct format.
